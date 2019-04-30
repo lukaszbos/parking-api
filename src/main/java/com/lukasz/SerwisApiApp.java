bf679062663fb1b7e35262d8b21a2464a992d0fe
Luc Mic <lukaszm39@gmail.com>
2019-04-30 02:29:07 +0200

    heroku

--- src/main/java/com/lukasz/SerwisApiApp.java
+++ src/main/java/com/lukasz/SerwisApiApp.java
@@ -1,11 +1,23 @@
 package com.lukasz;
 
+import com.lukasz.parking.Parking;
+import com.lukasz.parking.ParkingRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SerwisApiApp {
     public static void main(String[] args) {
         SpringApplication.run(SerwisApiApp.class, args);
     }
+
+    @Bean
+    ApplicationRunner applicationRunner(ParkingRepository parkingRepository) {
+        return args -> {
+            parkingRepository.save(new Parking(1, "parking"));
+        };
+    }
+
 }
